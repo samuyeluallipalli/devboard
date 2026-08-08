27a13409ab2ab5d48bc8212d300e70af3c497e77
samuyeluallipalli <samuelallipalli@gmail.com>
2026-08-08 23:56:40 +0530

    Set dark mode as default

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true,
+    baseURL: "/api",
+    withCredentials: true,
 });
 
 export default client;
